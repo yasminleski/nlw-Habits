075218c1e7997bd2aab2e5c320d2e246998b246a
yasmin.leski <yasmin.leski@tecnospeed.com.br>
2023-04-14 18:19:08 -0300

    criação do process bar

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,22 @@
+import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
+
 export function HabitDay() {
-    return (
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
-        </div>
-    )
+  return (
+    <Popover.Root>
+      <Popover.Trigger
+        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">sexta-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">14/04</span>
+
+          <ProgressBar progress={12} />
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  )
 }
